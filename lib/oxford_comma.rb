cebad95fcf097ac66c62d517614d2ca70c63974c
DavidNNussbaum <brocknynj@aol.com>
2020-10-27 11:26:45 -0400

    commit tree f9c1b9e9d9bdb82a20e49fdb7b30d555f190629f

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,25 @@
 def oxford_comma(array)
-
+    if array.length == 1
+        str = array.join 
+        str
+    
+    elsif array.length == 2
+        array.insert(-2, "and")
+        str = array.join(" ")  
+        # str = str.insert(-2, "and") 
+        str
+    elsif array.length == 3
+        array.insert(-2, "and")
+        str = array.join(", ").sub("and,", "and")  
+        # str = str.insert(-2, "and") 
+        str
+    
+    else
+        str = array.join 
+        str = array.insert(-2, "and")
+        str = array.join(", ").sub("and,", "and") 
+        # str = array.join(" ")  
+        # str = [str[0...-1].join(", "), str.last].join(", and ")
+        str
+    end  
 end
